06df47d56a169f5b35dfcb6bcdd99109b89bfca6
Robledo Castro <bedocastro@gmail.com>
2025-04-23 10:52:19 -0300

    Configurando authorize com hasRole e hasAuthority

--- src/main/java/io/github/robledocastro/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/robledocastro/libraryapi/config/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package io.github.robledocastro.libraryapi.config;
 import ch.qos.logback.core.pattern.color.BoldCyanCompositeConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,10 +26,17 @@ public class SecurityConfiguration {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(configurer -> {
-                    configurer.loginPage("/login").permitAll();
+                    configurer.loginPage("/login");
                 })
                 .httpBasic(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/login").permitAll();
+                    authorize.requestMatchers(HttpMethod.POST, "/autores/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.PUT, "/autores/**").hasRole("ADMIN");
+                    authorize.requestMatchers(HttpMethod.DELETE, "/autores/**").hasRole("ADMIN");
+                    //authorize.requestMatchers(HttpMethod.DELETE, "/autores/**").hasAuthority("DELETAR-USUARIO");
+                    authorize.requestMatchers(HttpMethod.GET, "/autores/**").hasAnyRole("USER", "ADMIN");
+                    authorize.requestMatchers("/livros/**").hasAnyRole("USER", "ADMIN");
                     authorize.anyRequest().authenticated();
                 })
                 .build();
