966f885fdd13da254be07a67a1e45f53105486bb
Robledo Castro <bedocastro@gmail.com>
2025-04-25 09:30:42 -0300

    Criando novos usuarios a partir da autenticacao com o Google

--- src/main/java/io/github/robledocastro/libraryapi/security/LoginSocialSuccessHandler.java
+++ src/main/java/io/github/robledocastro/libraryapi/security/LoginSocialSuccessHandler.java
@@ -14,10 +14,13 @@ import org.springframework.security.web.authentication.SavedRequestAwareAuthenti
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class LoginSocialSuccessHandler extends SavedRequestAwareAuthenticationSuccessHandler {
+
+    private static final String SENHA_PADRAO = "s123";
 
     private final UsuarioService usuarioService;
 
@@ -34,10 +37,30 @@ public class LoginSocialSuccessHandler extends SavedRequestAwareAuthenticationSu
 
         Usuario usuario = usuarioService.obterPorEmail(email);
 
+        if(usuario == null){
+            usuario = cadastrarUsuarioNaBase(email);
+        }
+
         CustomAuthentication customAuthentication = new CustomAuthentication(usuario);
 
         SecurityContextHolder.getContext().setAuthentication(customAuthentication);
 
         super.onAuthenticationSuccess(request, response, authentication);
+    }
+
+    private Usuario cadastrarUsuarioNaBase(String email) {
+        Usuario usuario;
+        usuario = new Usuario();
+        usuario.setEmail(email);
+        usuario.setLogin(obterLoginComEmail(email));
+        usuario.setSenha(SENHA_PADRAO);
+        usuario.setRoles(List.of("OPERADOR"));
+
+        usuarioService.salvar(usuario);
+        return usuario;
+    }
+
+    private String obterLoginComEmail(String email) {
+        return email.substring(0, email.indexOf("@"));
     }
 }
